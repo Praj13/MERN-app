1a2a739cec617aca7c6e68712c92991aaefcb571
Praj13 <rajbansiprajwol@gmail.com>
2025-02-14 17:03:59 +0545

    add and edit part completed

--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const AddEditNotes = () => {
   return (
     <div>
-      AddEditNotes
+      <div className='flex flex-col gap-2'>
+        <label className='text-xs text-slate-400'>Title</label>
+        <input type="text" placeholder='Go to Gym at 5'  className='text-2xl text-slate-950 outline-none'/>
+
+      </div>
+      <div className='flex flex-col gap-2 mt-4'>
+        <label className='text-xs text-slate-400' >CONTENT</label>
+        <textarea name="" id="" type='text' className='text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded' placeholder='Content' rows={10}></textarea>
+      </div>
+      <div className='mt-3'>
+        <label className='text-xs text-slate-400'>Tags</label> 
+       <TagInput/>
+        </div>
+        <button className='w-full bg-blue-500 font-medium mt-5 p-3' onClick={()=>{
+             
+        }}>ADD</button>
+      
     </div>
   )
 }
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NoteCard from '../../components/Cards/NoteCard'
 import Navbar from '../../components/Navbar/Navbar'
 import { MdDelete, MdCreate, MdAdd } from 'react-icons/md'
 import AddEditNotes from './AddEditNotes'
+import Modal from "react-modal";
 
 
 const Home = () => {
+  const [openAddEditModal,setOpenAddEditModal]=useState({
+    isShown:false,
+    type:"add",
+    data:null,
+  })
   return (
    <>
   <Navbar/>
@@ -31,10 +37,24 @@ const Home = () => {
     </div>
   </div>
 <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-blue-500 hover:bg-blue-600 text-white absolute right-10 bottom-10' onClick={()=>{
-
+setOpenAddEditModal({isShown:true, type:"add", data:null})
 }}><MdAdd className='text-[32px text-white]'/></button>
+  <Modal
+  isOpen={openAddEditModal.isShown}
+  onRequestClose={()=>{
+   
+  }}
+  style={{
+      overlay:{
+        backgroundColor:"rgba(0,0,0,0.2)",
+      },
+  }}
+  contentLabel=""
+  className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+  >
 
 <AddEditNotes/>
+</Modal>
    </>
   )
 }
