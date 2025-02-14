44e96e21844e6f7425c9e77d894732461fb1429b
Praj13 <rajbansiprajwol@gmail.com>
2025-02-14 15:51:08 +0545

    add and edit page

---
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddEditNotes = () => {
+  return (
+    <div>
+      AddEditNotes
+    </div>
+  )
+}
+
+export default AddEditNotes
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import NoteCard from '../../components/Cards/NoteCard'
 import Navbar from '../../components/Navbar/Navbar'
+import { MdDelete, MdCreate, MdAdd } from 'react-icons/md'
+import AddEditNotes from './AddEditNotes'
 
 
 const Home = () => {
@@ -7,10 +10,31 @@ const Home = () => {
    <>
   <Navbar/>
   <div className='container mx-auto'>
-    <NoteCard/>
+    <div className='grid grid-cols-3 gap-4 mt-8'>
+    <NoteCard
+    title="Meeting"
+    date="20th April 2025"
+    content="Meeting with client"
+     tags="#meeting"        
+      isPinned={true}
+      onEdit={()=>{
 
+      }}
+      onDelete={()=>{
+
+      }}
+       onPinNote={()=>{
+
+       }}
+    />
+
+    </div>
   </div>
+<button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-blue-500 hover:bg-blue-600 text-white absolute right-10 bottom-10' onClick={()=>{
 
+}}><MdAdd className='text-[32px text-white]'/></button>
+
+<AddEditNotes/>
    </>
   )
 }
